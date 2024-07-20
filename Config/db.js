80ebcfa641e062b75473baccd49396e0dceaf732
lijitht4107 <lijiththazhathethil@gmail.com>
2024-07-21 00:53:10 +0530

    db modified

--- Config/db.js
+++ Config/db.js
@@ -1,15 +1,18 @@
 const mongoose=require('mongoose')
 const connectDb= async()=>{
-
-     try { 
-        const connection=await mongoose.connect('mongodb+srv://lijiththazhathethil:qbsBSoOB8t3PEVUw@cluster0.gf4ke0g.mongodb.net/',{
-            useNewUrlParser:'true'
-        })
-        console.log("Mongodb data base connected");
-        
-     } catch (err) {
-        console.log( err,"not connected");
-        
-     }
+      await mongoose.connect('mongodb+srv://lijiththazhathethil:r1gpJji93bRMmeQN@cluster0.mpf3kp0.mongodb.net/playGround')
 }
+connectDb().then(console.log('connected to db')).catch(err => console.log(err))
+
+   //   try { 
+   //      const connection=await mongoose.connect('mongodb+srv://lijiththazhathethil:qbsBSoOB8t3PEVUw@cluster0.gf4ke0g.mongodb.net/',{
+   //          useNewUrlParser:'true'
+   //      })
+   //      console.log("Mongodb data base connected");
+        
+   //   } catch (err) {
+   //      console.log( err,"not connected");
+        
+   //   }
+// }
 module.exports=connectDb 
--- app.js
+++ app.js
@@ -1,60 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const connectDb = require('./Config/db');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
+const createError = require('http-errors');
+const connectDb = require('./Config/db');
 require('dotenv').config();
-var app = express();
+const app = express();
 
 app.use(cors({
-  origin:['https://bookacourt-app.onrender.com','http://localhost:3000']
-}))
+  origin: ['https://bookacourt-app.onrender.com', 'http://localhost:3000']
+}));
 
-// console.log(process.env.JWT_PASSWORD)
+// Load routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/authRouter');
+const adminRoute = require('./routes/adminRoute');
+const paymentRoute = require('./routes/paymentRoute');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/authRouter')
-const adminRoute=require('./routes/adminRoute')
-const paymentRoute=require('./routes/paymentRoute')
-
-
-
+// Connect to database
 connectDb();
 
-
-// view engine setup
+// Set view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/admin', adminRoute);
-app.use('/payment',paymentRoute)
+app.use('/payment', paymentRoute);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set the port
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
